1ba70667b8ee62dea2172d1d07d443f93d725cfb
Wei9023 <vv880923@gmail.com>
2019-04-16 10:12:38 -0700

    got the event body

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const LocalStorage = require('node-localstorage').LocalStorage;
 const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
 const http = require('http');
 const express = require('express');
-
+const request = require('request');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+const superagent = require('superagent');
 // *** Initialize event adapter using signing secret from environment variables ***
 const slackEvents = slackEventsApi.createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
   includeBody: true
 });
-
+console.log('abcdefg received:');
 // Initialize a Local Storage object to store authorization info
 // NOTE: This is an insecure method and thus for demo purposes only!
 const botAuthorizationStorage = new LocalStorage('./storage');
@@ -71,6 +74,9 @@ app.use('/slack/events', slackEvents.expressMiddleware());
 
 // *** Greeting any user that says "hi" ***
 slackEvents.on('message', (message, body) => {
+  console.log('message received:');
+  console.log('message type:' + message.subtype);
+  console.log('message:' + message.text);
   // Only deal with messages that have no subtype (plain messages) and contain 'hi'
   if (!message.subtype && message.text.indexOf('clean') >= 0) {
     // Initialize a client
@@ -83,7 +89,38 @@ slackEvents.on('message', (message, body) => {
     slack.chat.postMessage({ channel: message.channel, text: `I am too tired to clean <@${message.user}>! :tired_face:` })
       .catch(console.error);
   }
+
+  
+
+
 });
+
+
+function handle_file_event(event, body){
+
+  console.log('event==>:');
+  console.log(event);
+  console.log(event.file_id);
+  console.log('body==>:');
+  console.log(body);
+  let url = 'https://slack.com/api/files.info?file=' + event.file_id +'&token=' + process.env.SLACK_BOT_TOKEN;
+  console.log(url);
+  superagent.get(url).then( data => {
+        console.log(data.body);
+        console.log('data.body.content------------>');
+        // console.log(data.body.title);
+        console.log(data.body.content);
+        // console.log(data.body.timestamp);
+        //let gist_id_url = call_gist(),
+        //mongodb.save(,title, name,   , , , ,, , )  
+        //mongodb.search(fromTime, endTime, title:abcdefg)
+  });
+    
+}
+
+slackEvents.on('file_created', handle_file_event);
+slackEvents.on('file_change', handle_file_event);
+
 
 // *** Responding to reactions with the same emoji ***
 slackEvents.on('reaction_added', (event, body) => {
@@ -108,6 +145,82 @@ ${JSON.stringify(error.body)}`);
     console.error(`An error occurred while handling a Slack event: ${error.message}`);
   }
 });
+
+app.post('/slack/slash-commands/send-me-buttons', urlencodedParser, (req, res) =>{
+  console.log('send-me-buttons-----------');
+  res.status(200).end() // best practice to respond with empty 200 status code
+  var reqBody = req.body
+  var responseURL = reqBody.response_url
+  if (reqBody.token != process.env.VERIFICATION_TOKEN){
+      res.status(403).end("Access forbidden")
+  }else{
+      var message = {
+          "text": "This is your first interactive message",
+          "attachments": [
+              {
+                  "text": "Building buttons is easy right?",
+                  "fallback": "Shame... buttons aren't supported in this land",
+                  "callback_id": "button_tutorial",
+                  "color": "#3AA3E3",
+                  "attachment_type": "default",
+                  "actions": [
+                      {
+                          "name": "yes",
+                          "text": "yes",
+                          "type": "button",
+                          "value": "yes"
+                      },
+                      {
+                          "name": "no",
+                          "text": "no",
+                          "type": "button",
+                          "value": "no"
+                      },
+                      {
+                          "name": "maybe",
+                          "text": "maybe",
+                          "type": "button",
+                          "value": "maybe",
+                          "style": "danger"
+                      }
+                  ]
+              }
+          ]
+      }
+      sendMessageToSlackResponseURL(responseURL, message)
+    }
+  })
+
+  function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    console.log('sendMessageToSlackResponseURL blablalba');
+    var postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error){
+            // handle errors as you see fit
+        }
+    })
+}
+
+app.post('/slack/actions', urlencodedParser, (req, res) =>{
+
+  console.log('/slack/actions blablalba');
+
+  res.status(200).end() // best practice to respond with 200 status
+  var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
+  var message = {
+      "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
+      "replace_original": false
+  }
+  sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
+})
+   
 
 // Start the express application
 const port = process.env.PORT || 3000;
