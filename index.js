c540e8718ee5367b40f7024f503ea2ab2f93d43a
Erin Trainor <trainor.erin@gmail.com>
2019-04-15 18:36:11 -0700

    current version

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express');
 
 // *** Initialize event adapter using signing secret from environment variables ***
 const slackEvents = slackEventsApi.createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
-  includeBody: true
+  includeBody: true,
 });
 
 // Initialize a Local Storage object to store authorization info
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
   res.send('<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
 });
 app.get('/auth/slack', passport.authenticate('slack', {
-  scope: ['bot']
+  scope: ['bot'],
 }));
 app.get('/auth/slack/callback',
   passport.authenticate('slack', { session: false }),
@@ -69,7 +69,7 @@ app.use('/slack/events', slackEvents.expressMiddleware());
 
 // *** Attach listeners to the event adapter ***
 
-// *** Greeting any user that says "hi" ***
+// *** Greeting any user that says "clean" ***
 slackEvents.on('message', (message, body) => {
   // Only deal with messages that have no subtype (plain messages) and contain 'hi'
   if (!message.subtype && message.text.indexOf('clean') >= 0) {
@@ -81,6 +81,143 @@ slackEvents.on('message', (message, body) => {
     }
     // Respond to the message back in the same channel
     slack.chat.postMessage({ channel: message.channel, text: `I am too tired to clean <@${message.user}>! :tired_face:` })
+      .catch(console.error);
+  }
+});
+
+slackEvents.on('file_created', (file, body) => {
+  // Only deal with messages that have no subtype (plain messages) and contain 'hi'
+  // if (!message.subtype && message.text.indexOf('```') >= 0) {
+  console.log('file-creation:', file);
+  // Initialize a client
+  // const slack = getClientByTeamId(body.team_id);
+  // // Handle initialization failure
+  // if (!slack) {
+  //   return console.error('No authorization found for this team. Did you install the app through the url provided by ngrok?');
+  // }
+  // // Respond to the message back in the same channel
+  // slack.chat.postMessage({ channel: message.channel, text: `hello... i made a file` })
+  //   .catch(console.error);
+  // }
+});
+
+// *** Greeting any user that says "```" ***
+slackEvents.on('message', (message, body) => {
+  // Only deal with messages that have no subtype (plain messages) and contain 'hi'
+  if (!message.subtype && message.text.indexOf('```') >= 0) {
+    console.log('backtick message:', message);
+    // Initialize a client
+    const slack = getClientByTeamId(body.team_id);
+    // Handle initialization failure
+    if (!slack) {
+      return console.error('No authorization found for this team. Did you install the app through the url provided by ngrok?');
+    }
+    // Respond to the message back in the same channel
+    slack.chat.postMessage({ channel: message.channel, blocks: [
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': 'I saw that you posted a code snippet!\n\n*Do you want to save it as a Gist on GitHub?*',
+        },
+      },
+      {
+        'type': 'actions',
+        'elements': [
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': 'Yes',
+            },
+            'value': 'click_me_123',
+            'style': 'primary',
+          },
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': 'No',
+            },
+            'value': 'click_me_123',
+            'style': 'danger',
+          },
+        ],
+      },
+      {
+        'type': 'divider',
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '*Here is the information I will save for you...*',
+        },
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '*File name:*\nmy-amazing-gist\n\n*Author:*\n',
+        },
+      },
+      {
+        'type': 'actions',
+        'elements': [
+          {
+            'type': 'users_select',
+            'placeholder': {
+              'type': 'plain_text',
+              'text': 'Select a user',
+              'emoji': true,
+            },
+          },
+        ],
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '*Choose a category*',
+        },
+        'accessory': {
+          'type': 'static_select',
+          'placeholder': {
+            'type': 'plain_text',
+            'text': 'Select a category',
+            'emoji': true,
+          },
+          'options': [
+            {
+              'text': {
+                'type': 'plain_text',
+                'text': 'Data Structures',
+                'emoji': true,
+              },
+              'value': 'value-0',
+            },
+            {
+              'text': {
+                'type': 'plain_text',
+                'text': 'Login Instructions',
+                'emoji': true,
+              },
+              'value': 'value-1',
+            },
+            {
+              'text': {
+                'type': 'plain_text',
+                'text': 'Random Stuff',
+                'emoji': true,
+              },
+              'value': 'value-2',
+            },
+          ],
+        },
+      },
+    ] })
       .catch(console.error);
   }
 });
@@ -97,6 +234,8 @@ slackEvents.on('reaction_added', (event, body) => {
   slack.chat.postMessage({ channel: event.item.channel, text: `:${event.reaction}:` })
     .catch(console.error);
 });
+
+
 
 // *** Handle errors ***
 slackEvents.on('error', (error) => {
